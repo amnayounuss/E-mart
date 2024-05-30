0306bc85379e8ddeb53996700beae0b9248a8859
amnayounus <amnayounus999@gmail.com>
2024-05-30 23:58:10 +0500

    logoChanged

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import { IoMdSearch } from "react-icons/io";
@@ -29,17 +30,14 @@ const MenuLinks = [
 const Navbar = () => (
   <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
     <div className="py-4">
-      <div className="container flex justify-between
-      item-center">
+      <div className="container flex justify-between items-center">
         {/* Logo */}
-        <div className="flex item-center gap-4">
-          <a
-            href="#"
-            className="text-primary font-semibold 
-            tracking-widest text-2xl uppercase 
-            sm:text-3xl"
-          >
-            E-Mart
+        <div className="flex items-center gap-4">
+          <a href="#" className="flex items-center gap-2">
+            <img src="logo.png" alt="E-Mart Logo" className="h-8 sm:h-10" />
+            {/* <span className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl">
+              E-Mart
+            </span> */}
           </a>
           {/* Menu Items */}
           <div className="hidden lg:block">
@@ -47,25 +45,22 @@ const Navbar = () => (
               {MenuLinks.map((data, index) => (
                 <li key={index}>
                   <a href={data.link}
-                    className="inline-block px-4
-                  font-semibold text-gray-500
-                  hover:text-black
-                  dark:hover:text-white duration-200"
-                  >{data.name}</a>
+                    className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200"
+                  >
+                    {data.name}
+                  </a>
                 </li>
               ))}
 
               {/* Dropdown */}
-              <li className="relai=tive cursor-pointer group">
+              <li className="relative cursor-pointer group">
                 <a href="#" className="flex items-center gap-[2px] font-semibold text-gray-500 dark:hover:text-white py-2">
                   Quick Links
                   <span>
-                  <FaCaretDown 
-                  className="group-hover:rotate-180 duration-300 " />
-                </span>
+                    <FaCaretDown className="group-hover:rotate-180 duration-300 " />
+                  </span>
                 </a>
                 <div></div>
-                
               </li>
             </ul>
           </div>
@@ -79,16 +74,14 @@ const Navbar = () => (
               className="search-bar"
             />
             <IoMdSearch
-              className="text-xl text-gray-600 group-hover:text-primary 
-            dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200"
+              className="text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200"
             />
           </div>
 
-          {/*Order-Button Section */}
+          {/* Order-Button Section */}
           <button className="relative p-3">
             <FaCartShopping className="text-xl text-gray-600 dark:text-gray-400" />
-            <div className="w-4 h-4 bg-red-500 text-whi
-              rounded-full absolute top-0 right-0 flex item-center justify-center text-xs">
+            <div className="w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs">
               4
             </div>
           </button>
